f33f66bd55bdea8ff51355e3f91f0ccc485502b4
Yuk1n0 <39364985+Yuk1n0@users.noreply.github.com>
2021-07-10 00:56:08 +0800

    Chore

--- shadowsocks.sh
+++ shadowsocks.sh
@@ -76,10 +76,10 @@ r_ciphers=(
     aes-128-ctr
     chacha20-ietf
 )
+
 # Reference URL:
 # https://github.com/shadowsocksr-rm/shadowsocks-rss/blob/master/ssr.md
 # https://github.com/shadowsocksrr/shadowsocksr/commit/a3cf0254508992b7126ab1151df0c2f10bf82680
-
 protocols=(
     origin
     verify_deflate
@@ -285,17 +285,6 @@ download_files() {
     fi
 }
 
-error_detect_depends() {
-    local command=$1
-    local depend=$(echo "${command}" | awk '{print $4}')
-    echo -e "[${green}Info${plain}] Starting to install package ${depend}"
-    ${command} >/dev/null 2>&1
-    if [ $? -ne 0 ]; then
-        echo -e "[${red}Error${plain}] Failed to install ${red}${depend}${plain}"
-        exit 1
-    fi
-}
-
 config_firewall() {
     if centosversion 6; then
         /etc/init.d/iptables status >/dev/null 2>&1
@@ -396,6 +385,17 @@ EOF
     fi
 }
 
+error_detect_depends() {
+    local command=$1
+    local depend=$(echo "${command}" | awk '{print $4}')
+    echo -e "[${green}Info${plain}] Starting to install package ${depend}"
+    ${command} >/dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        echo -e "[${red}Error${plain}] Failed to install ${red}${depend}${plain}"
+        exit 1
+    fi
+}
+
 install_dependencies() {
     if check_sys packageManager yum; then
         echo -e "[${green}Info${plain}] Checking the EPEL repository..."
@@ -462,7 +462,7 @@ install_select() {
             echo
             echo "You choose = ${software[${selected} - 1]}"
             if [ "${selected}" == "1" ]; then
-                echo -e "[${green}Info${plain}] Current official Shadowsocks-libev Version: ${libev_ver}"
+                echo -e "[${green}Info${plain}] Shadowsocks-libev Version: ${libev_ver}"
             fi
             echo
             break
@@ -654,6 +654,7 @@ get_cert() {
         fi
     fi
 }
+
 get_char() {
     SAVEDSTTY=$(stty -g)
     stty -echo
@@ -906,7 +907,7 @@ uninstall_shadowsocks_libev() {
             update-rc.d -f ${service_name} remove
         fi
         if [ "${answer_upgrade}" != "y" ] || [ "${answer_upgrade}" != "Y" ]; then
-            rm -fr $(dirname ${shadowsocks_libev_config})
+            rm -rf $(dirname ${shadowsocks_libev_config})
             rm -f /usr/local/bin/v2ray-plugin
         fi
         rm -f /usr/local/bin/ss-local
@@ -927,7 +928,6 @@ uninstall_shadowsocks_libev() {
         rm -f /usr/local/share/man/man1/ss-nat.1
         rm -f /usr/local/share/man/man8/shadowsocks-libev.8
         rm -rf /usr/local/share/doc/shadowsocks-libev
-        rm -rf /etc/shadowsocks-libev
         rm -f ${shadowsocks_libev_init}
         echo -e "[${green}Info${plain}] ${software[0]} uninstall success"
     else
@@ -952,10 +952,10 @@ uninstall_shadowsocks_r() {
         elif check_sys packageManager apt; then
             update-rc.d -f ${service_name} remove
         fi
-        rm -fr $(dirname ${shadowsocks_r_config})
+        rm -rf $(dirname ${shadowsocks_r_config})
         rm -f ${shadowsocks_r_init}
         rm -f /var/log/shadowsocks.log
-        rm -fr /usr/local/shadowsocks
+        rm -rf /usr/local/shadowsocks
         echo -e "[${green}Info${plain}] ${software[1]} uninstall success"
     else
         echo
@@ -1027,7 +1027,7 @@ upgrade_shadowsocks() {
             get_libev_ver
             current_libev_ver=$(echo ${libev_ver} | sed -e 's/^[a-zA-Z]//g')
             echo
-            echo -e "[${green}Info${plain}] Current official Shadowsocks-libev Version: v${current_local_version}"
+            echo -e "[${green}Info${plain}] Shadowsocks-libev Version: v${current_local_version}"
             if [[ "${current_libev_ver}" == "${current_local_version}" ]]; then
                 echo
                 echo -e "[${green}Info${plain}] Already updated to latest version !"
